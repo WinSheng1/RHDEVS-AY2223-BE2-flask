f503d2265ef4b4de9abb3eb89fc00c2e8f90617b
Lim Win Sheng <limwinsheng@gmail.com>
2022-10-14 00:01:34 +0800

    first commit

--- Profiles/ProfilesAPI.py
+++ Profiles/ProfilesAPI.py
@@ -1,6 +1,61 @@
-from flask import Blueprint
+from flask import Blueprint, request, make_response
 import sys
+from flask_cors import cross_origin
 from db import db
 sys.path.append("../")
 
 profiles_api = Blueprint("profiles", __name__)
+
+@profiles_api.route("/<int:id>", methods = ["GET"])
+@cross_origin(supports_credentials=True)
+def getProfile(id):
+    try:
+        data = {"name": db[id]["name"], "scores": db[id]["scores"]}
+        response = {"status": "success", "data": data}
+
+    except Exception as e:
+        return {"err": "An error has occured", "status": "failed"}, 500
+
+    return make_response(response)
+@profiles_api.route("/profiles", methods=["POST"])
+@cross_origin(supports_credentials=True)
+def newProfile():
+    try:
+        data = request.get_json()
+        db.append(data)
+        response = {"status": "success"}
+    except Exception as e:
+        return {"err": "An error has occured", "status": "failed"}, 500
+
+    return make_response(response)
+
+
+@profiles_api.route("/<int:id>", methods=["DELETE"])
+@cross_origin(supports_credentials=True)
+def delete_profile(id):
+    try:
+        id = int(id)
+        deleted = db[id]
+        new_db = [profile for profile in db if profile != deleted]
+        response = {"deleted": deleted, "new_db": new_db, "status": "success"}
+    except Exception as e:
+        return {"err": "An error has occured", "status": "failed"}, 500
+
+    return make_response(response)
+
+
+@profiles_api.route("/<int:id>/score", methods=["GET"])
+@cross_origin(supports_credentials=True)
+def get_min_score(id):
+    try:
+        minScore = request.args.get("minScore")
+        if minScore is None:
+            data = {"scores": db[id]["scores"]}
+        else:
+            minScore = int(minScore)
+            data = {"scores": [score for score in db[id]["scores"] if score > minScore]}
+        response = {"status": "success", "data": data}
+    except Exception as e:
+        return {"err": "An error has occured", "status": "failed"}, 500
+
+    return make_response(response)
--- main.py
+++ main.py
@@ -3,10 +3,14 @@ from Profiles.ProfilesAPI import profiles_api
 from flask import Flask
 from db import db
 
-
-# Write your flask code here
-
 app = Flask(__name__)
 
 app.register_blueprint(profiles_api, url_prefix="/profiles")
 app.register_blueprint(auth_api, url_prefix="/auth")
+
+@app.route("/", methods = ["GET"])
+def hello():
+    return "Welcome!"
+
+if __name__ == "__main__":
+    app.run()
